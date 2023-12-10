e3986324fdfb6cc5c1e2eb6355a2ad40a25990d0
ethangilmore <ethanmckgilmore@gmail.com>
2023-12-09 17:15:36 -0700

    Updated dash direction logic

--- Assets/Scripts/EthanPlayerMovement.cs
+++ Assets/Scripts/EthanPlayerMovement.cs
@@ -24,9 +24,9 @@ public class EthanPlayerMovement : MonoBehaviour
     [Range(0,1)] [SerializeField] private float _airControl = 0.2f;
     [SerializeField] private float _jumpCooldown = 0.1f;
 
-    public Vector3 Velocity = Vector3.zero;
-    public bool Grounded = false;
-    public bool Dashing = false;
+    [HideInInspector] public Vector3 Velocity = Vector3.zero;
+    [HideInInspector] public bool Grounded = false;
+    [HideInInspector] public bool Dashing = false;
     private Vector3 _dashDirection = Vector3.zero;
     private float _dashTimer = 0f;
     private float _jumpTimer = 0f;
@@ -55,7 +55,7 @@ public class EthanPlayerMovement : MonoBehaviour
         // Check for jump and dash
         _dashTimer -= Time.deltaTime;
         _jumpTimer -= Time.deltaTime;
-        if (Input.GetKeyDown(KeyCode.Space) && _jumpTimer <= 0)
+        if (Input.GetKeyDown(KeyCode.Space) && _jumpTimer <= 0 && Grounded)
         {
             Jump();
         }
@@ -80,8 +80,9 @@ public class EthanPlayerMovement : MonoBehaviour
         Vector3 actualVelocity = Vector3.ProjectOnPlane(Velocity, Vector3.up);
         Velocity += (desiredVelocity - actualVelocity) * availableAcceleration * Time.deltaTime;
 
-        // Apply gravity
-        Velocity += Vector3.up * Physics.gravity.y * 2.5f * Time.deltaTime;
+        // Gravity
+        if (!Grounded)
+            Velocity += Vector3.up * Physics.gravity.y * 2.5f * Time.deltaTime;
         if (Dashing)
             Velocity = Vector3.ProjectOnPlane(Velocity, Vector3.up);
 
@@ -98,7 +99,12 @@ public class EthanPlayerMovement : MonoBehaviour
     {
         Dashing = true;
         Vector3 input = transform.forward * Input.GetAxisRaw("Vertical") + transform.right * Input.GetAxisRaw("Horizontal");
-        _dashDirection = input.magnitude > .1f ? input.normalized : transform.forward;
+        if (input.magnitude > .1f)
+            _dashDirection = input.normalized;
+        else if (Vector3.ProjectOnPlane(Velocity, Vector3.up).magnitude > MaxSpeed / 2)
+            _dashDirection = Vector3.ProjectOnPlane(Velocity, Vector3.up).normalized;
+        else
+            _dashDirection = transform.forward;
         yield return new WaitForSeconds(_dashDuration);
         Dashing = false;
         _dashTimer = _dashCooldown;
