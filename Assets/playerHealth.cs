10e899f13296ec29aa9754053b41760628662f22
Hayes Geldmacher <hayes.g850@gmail.com>
2023-12-12 19:39:34 -0700

    added player on death

--- Assets/playerHealth.cs
+++ Assets/playerHealth.cs
@@ -7,20 +7,17 @@ public class playerHealth : MonoBehaviour
 {
     [SerializeField] private TMP_Text _healthText;
     [SerializeField] private Animator _healthAnimator;
-
     [SerializeField] private Health _playerHealth;
 
    void Start()
     {
-        Debug.Log("Started!");
         _playerHealth.OnDamageTaken += DamageUpdateText;
         _playerHealth.SetMaxHealth += StartUpdateText;
+        _playerHealth.OnDeath += OnDeathPlayer;
     }
 
     private void StartUpdateText(float oldHealth, float newHealth)
     {
-
-
         //Sets the health UI to reflect playerHealth
         _healthText.text = newHealth.ToString();
     }
@@ -30,6 +27,12 @@ public class playerHealth : MonoBehaviour
         //Sets the health UI to reflect playerHealth
         _healthText.text = newHealth.ToString();
         _healthAnimator.SetTrigger("hurt");
+    }
+
+    private void OnDeathPlayer()
+    {
+        //this is where the player will die!
+        Debug.Log("playerDied!");
     }
 
  
