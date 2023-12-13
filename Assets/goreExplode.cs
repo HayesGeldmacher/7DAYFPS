6ac291166b2db829d5e41d4ca858f934c1d827db
Hayes Geldmacher <hayes.g850@gmail.com>
2023-12-13 01:39:07 -0700

    more work on corpse prefab

--- Assets/goreExplode.cs
+++ Assets/goreExplode.cs
@@ -8,10 +8,18 @@ public class goreExplode : MonoBehaviour
     [SerializeField] private float lineLifeSpan = 2f;
     [SerializeField] private float lowerBounds = 200f;
     [SerializeField] private float upperBounds = 400f;
+    [SerializeField] private float _fadeSpeed;
+    private float _currentFade;
+
+
+    private Material _material;
 
     // Start is called before the first frame update
     void Start()
     {
+        _currentFade = 1;
+        _material = GetComponent<MeshRenderer>().material;
+        
         //Randomly adds a force to each gib and throws the around the arena
 
         float shootX = Random.Range(lowerBounds, upperBounds);
@@ -25,5 +33,13 @@ public class goreExplode : MonoBehaviour
         rb.AddForce(new Vector3(finalShootX, finalShootY, finalShootZ));
     }
 
+    void Update()
+    {
+        _currentFade += (Time.deltaTime * -_fadeSpeed);
+        _currentFade = Mathf.Clamp(_currentFade, 0,1);
+        _material.SetFloat("Vector1_4fd90d0ce6d349c9a6400ec21704a612", _currentFade);
+
+
+    }
 
 }
