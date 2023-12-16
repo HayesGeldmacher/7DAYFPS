5918690018c52eef611c91395f7e730d4369c307
Hayes Geldmacher <hayes.g850@gmail.com>
2023-12-16 16:23:06 -0700

    Added slowDown effect

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -6,14 +6,56 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private TMP_Text _timeText;
+    private bool _isSlowedDown = false;
+    private float _pauseEndTime = 0;
     public float TimeElapsed { get; private set; } = 0f;
-    
-    
+
+    #region Singleton
+
+    public static GameManager instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("More than one instance of inventory present!! NOT GOOD!");
+            return;
+        }
+
+        instance = this;
+    }
+
+    #endregion
+
 
     // Update is called once per frame
     void Update()
     {
         TimeElapsed += Time.deltaTime;
         _timeText.text = TimeElapsed.ToString("F2");
+
+        
+        //counts real time to check when to stop slow motion
+        if (_isSlowedDown)
+        {
+            if (Time.realtimeSinceStartup > _pauseEndTime)
+            {
+                Debug.Log("BackToNormal");
+                Time.timeScale = 1f;
+                _isSlowedDown = false;
+            }
+        }
+    }
+
+    public IEnumerator SlowMotion(float time)
+    {
+
+        Time.timeScale = 0.2f;
+        _isSlowedDown = true;
+        _pauseEndTime = Time.realtimeSinceStartup + time;
+
+
+        yield return new WaitForSeconds(0.1f);
+
     }
 }
--- Assets/Scripts/Health and Damage/DestroyOnDeath.cs
+++ Assets/Scripts/Health and Damage/DestroyOnDeath.cs
@@ -9,6 +9,9 @@ public class DestroyOnDeath : MonoBehaviour
     [SerializeField] private GameObject _corpse;
     [SerializeField] private bool _playAudio;
     [SerializeField] private GameObject _audioEffect;
+    [SerializeField] private bool _canSlow;
+    [SerializeField] private float _slowTime;
+
 
     private void OnEnable()
     {
@@ -32,6 +35,17 @@ public class DestroyOnDeath : MonoBehaviour
             Instantiate(_audioEffect, transform.position, Quaternion.identity);
         }
 
+        if (_canSlow)
+        {
+            StartSlowMotion();
+        }
+
         Destroy(gameObject);
+    }
+
+    private void StartSlowMotion()
+    {
+        GameManager slow = GameManager.instance;
+        StartCoroutine(slow.SlowMotion(_slowTime));
     }
 }
