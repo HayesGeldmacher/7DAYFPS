b03aa2f0eef66f475711d93dee6dcac94eabe118
ethangilmore <ethanmckgilmore@gmail.com>
2023-12-16 15:33:45 -0700

    Redid tracking scripts

--- Assets/Enemies/BigWolf/Wolf.cs
+++ Assets/Enemies/BigWolf/Wolf.cs
@@ -15,85 +15,88 @@ public class Wolf : MonoBehaviour
     [SerializeField] private Animator _animator;
     private Transform _target;
     private Vector3 _velocity = Vector3.zero;
+    private Rigidbody _rigidbody;
     
     void Start()
     {
+        _rigidbody = GetComponent<Rigidbody>();
         _target = GameObject.FindObjectOfType<EthanPlayerMovement>().transform;
-        _animator.Play(_animator.GetCurrentAnimatorClipInfo(0)[0].clip.name, 0, Random.Range(0f, 1f));
     }
 
     void Update()
     {
-        // Get all other wolfs
-        Wolf[] wolfs = FindObjectsOfType<Wolf>();
+        // // Get all other wolfs
+        // Wolf[] wolfs = FindObjectsOfType<Wolf>();
         
-        // do boids
-        Vector3 cohesion = Vector3.zero;
-        Vector3 separation = Vector3.zero;
-        Vector3 alignment = Vector3.zero;
-        int cohesionCount = 0;
-        int separationCount = 0;
-        int alignmentCount = 0;
+        // // do boids
+        // Vector3 cohesion = Vector3.zero;
+        // Vector3 separation = Vector3.zero;
+        // Vector3 alignment = Vector3.zero;
+        // int cohesionCount = 0;
+        // int separationCount = 0;
+        // int alignmentCount = 0;
 
-        foreach (Wolf wolf in wolfs)
-        {
-            if (wolf != this)
-            {
-                float distance = Vector3.Distance(transform.position, wolf.transform.position);
-                if (distance < _cohesionRadius)
-                {
-                    cohesion += wolf.transform.position;
-                    cohesionCount++;
-                }
-                if (distance < _separationRadius)
-                {
-                    separation += transform.position - wolf.transform.position;
-                    separationCount++;
-                }
-                if (distance < _alignmentRadius)
-                {
-                    alignment += wolf._velocity;
-                    alignmentCount++;
-                }
-            }
-        }
+        // foreach (Wolf wolf in wolfs)
+        // {
+        //     if (wolf != this)
+        //     {
+        //         float distance = Vector3.Distance(transform.position, wolf.transform.position);
+        //         if (distance < _cohesionRadius)
+        //         {
+        //             cohesion += wolf.transform.position;
+        //             cohesionCount++;
+        //         }
+        //         if (distance < _separationRadius)
+        //         {
+        //             separation += transform.position - wolf.transform.position;
+        //             separationCount++;
+        //         }
+        //         if (distance < _alignmentRadius)
+        //         {
+        //             alignment += wolf._velocity;
+        //             alignmentCount++;
+        //         }
+        //     }
+        // }
 
-        if (cohesionCount > 0)
-        {
-            cohesion /= cohesionCount;
-            cohesion -= transform.position;
-        }
+        // if (cohesionCount > 0)
+        // {
+        //     cohesion /= cohesionCount;
+        //     cohesion -= transform.position;
+        // }
 
-        if (separationCount > 0)
-        {
-            separation /= separationCount;
-        }
+        // if (separationCount > 0)
+        // {
+        //     separation /= separationCount;
+        // }
 
-        if (alignmentCount > 0)
-        {
-            alignment /= alignmentCount;
-        }
+        // if (alignmentCount > 0)
+        // {
+        //     alignment /= alignmentCount;
+        // }
 
-        // add target
-        cohesion += _target.position - transform.position;
+        // // add target
+        // cohesion += _target.position - transform.position;
 
-        // apply weights
-        cohesion *= _cohesionWeight;
-        separation *= _separationWeight;
-        alignment *= _alignmentWeight;
+        // // apply weights
+        // cohesion *= _cohesionWeight;
+        // separation *= _separationWeight;
+        // alignment *= _alignmentWeight;
 
-        // add random noise
-        cohesion += Random.insideUnitSphere * _randomWeight;
-        separation += Random.insideUnitSphere * _randomWeight;
-        alignment += Random.insideUnitSphere * _randomWeight;
+        // // add random noise
+        // cohesion += Random.insideUnitSphere * _randomWeight;
+        // separation += Random.insideUnitSphere * _randomWeight;
+        // alignment += Random.insideUnitSphere * _randomWeight;
 
         // apply forces
-        _velocity += cohesion + separation + alignment;
+        // _velocity += cohesion + separation + alignment;
+        // transform.position += _velocity * Time.deltaTime;
+
+        _velocity += (_target.position - transform.position).normalized * _speed;
+        _velocity += Random.insideUnitSphere * _randomWeight;
         _velocity.y = 0;
         _velocity = Vector3.ClampMagnitude(_velocity, _speed);
-        transform.position += _velocity * Time.deltaTime;
         transform.rotation = Quaternion.LookRotation(_target.position - transform.position, Vector3.up);
-
-        
+        _rigidbody.velocity = _velocity;
     }
 }
---
+++ Assets/Enemies/Cicada/BasicTrackPlayer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicTrackPlayer : MonoBehaviour
+{
+    [SerializeField] private float _speed = 1f;
+    [SerializeField] private float _randomWeight = 3f;
+    [SerializeField] private bool _y = true;
+    private Transform _target;
+    private Vector3 _velocity = Vector3.zero;
+    private Rigidbody _rigidbody;
+    
+    void Start()
+    {
+        _target = FindObjectOfType<EthanPlayerMovement>().transform;
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        _velocity += (_target.position - transform.position).normalized * _speed;
+        _velocity += Random.insideUnitSphere * _randomWeight;
+        if (!_y)
+            _velocity.y = 0;
+        _velocity = Vector3.ClampMagnitude(_velocity, _speed);
+        transform.rotation = Quaternion.LookRotation(_target.position - transform.position, Vector3.up);
+        _rigidbody.velocity = _velocity;
+    }
+}
