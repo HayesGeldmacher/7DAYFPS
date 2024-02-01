efc9489ed730fa94a2eb30b86427fdad913c8f96
Hayes Geldmacher <hayes.g850@gmail.com>
2024-01-31 23:13:00 -0700

    Getting Jump and movement changes integrated

--- Assets/Scenes/Scripts/EthanPlayerMovement.cs
+++ Assets/Scenes/Scripts/EthanPlayerMovement.cs
@@ -19,7 +19,7 @@ public class EthanPlayerMovement : MonoBehaviour
     [SerializeField] private float _jumpVelocity = 10f;
     [SerializeField] private float _boostVelocity = 10f;
     [SerializeField] private float _boostAcceleration = 10f;
-    [Range(0,1)] [SerializeField] private float _airControl = 0.2f;
+    [Range(0, 1)][SerializeField] private float _airControl = 0.2f;
     [SerializeField] private float _jumpCooldown = 0.1f;
     [Header("Energy")]
     [SerializeField] private float _maxEnergy = 100f;
@@ -27,7 +27,7 @@ public class EthanPlayerMovement : MonoBehaviour
     [SerializeField] private float _energyCostDash = 8;
     [SerializeField] private float _energyCostIdle = 1;
     [SerializeField] private float _energyCostBoost = 4;
-    [SerializeField] private TMP_Text  _energyText;
+    [SerializeField] private TMP_Text _energyText;
     [Header("Audio")]
     [SerializeField] private float _minSoundVolume = 0.8f;
     [SerializeField] private float _maxSoundVolume = 0.12f;
@@ -37,8 +37,14 @@ public class EthanPlayerMovement : MonoBehaviour
     [SerializeField] private float _chargedJumpHeight;
     [SerializeField] private float _jumpHoldTime;
     [SerializeField] private float _currentJumpHoldTime;
-    [SerializeField ]private bool _chargedJump = false;
+    [SerializeField] private bool _chargedJump = false;
+    [SerializeField] private Animator _springAnim;
     public bool _turretMode = false;
+    [SerializeField] private Transform _cam;
+    [SerializeField] private Transform _camCrouch;
+    [SerializeField] private Transform _camOrigin;
+    [SerializeField] private AudioSource _jumpAudio;
+    [SerializeField] private float _jetPackStrength;
 
     private float _storedCamX;
     private float _storedCamY;
@@ -81,6 +87,7 @@ public class EthanPlayerMovement : MonoBehaviour
         UnityEngine.Cursor.visible = false;
         _currentJumpHoldTime = 0;
 
+
         _storedCamX = _camControl._mouseSensitivityX;
         _storedCamY = _camControl._mouseSensitivityY;
     }
@@ -89,46 +96,55 @@ public class EthanPlayerMovement : MonoBehaviour
     {
         if (GameManager.instance.PlayerDied) return;
 
-            if (_camControl._mouseSensitivityX != _storedCamX)
-            {
-                _camControl._mouseSensitivityX = _storedCamX;
-            }
-            if (_camControl._mouseSensitivityY != _storedCamY)
-            {
-                _camControl._mouseSensitivityY = _storedCamY;
-            }
+        if (_camControl._mouseSensitivityX != _storedCamX)
+        {
+            _camControl._mouseSensitivityX = _storedCamX;
+        }
+        if (_camControl._mouseSensitivityY != _storedCamY)
+        {
+            _camControl._mouseSensitivityY = _storedCamY;
+        }
 
-            float horizontal = Input.GetAxisRaw("Horizontal");
-            float vertical = Input.GetAxisRaw("Vertical");
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
 
-            // Check if the player is grounded
-            Grounded = Physics.Raycast(transform.position, Vector3.down, 1.5f);
+        // Check if the player is grounded
+        Grounded = Physics.Raycast(transform.position, Vector3.down, 1.5f);
 
-            // Check for jump and dash
-            _dashTimer -= Time.deltaTime;
-            _jumpTimer -= Time.deltaTime;
-            Boosting = false;
-            if (Input.GetKey(KeyCode.Space))
+        // Check for jump and dash
+        _dashTimer -= Time.deltaTime;
+        _jumpTimer -= Time.deltaTime;
+        Boosting = false;
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Boosting = true;
+
+            if (Input.GetKeyDown(KeyCode.Space) && _jumpTimer <= 0 && Grounded)
             {
-                Boosting = true;
-                if (Input.GetKeyDown(KeyCode.Space) && _jumpTimer <= 0 && Grounded)
-            {
-                   // Jump(false);
+                // Jump(false);
 
-            }
-                else if (Input.GetKey(KeyCode.Space))
-                {
-                    Boost();
-                }
-                
 
             }
+            else if (Input.GetKey(KeyCode.Space))
+            {
+                Boost();
+            }
+
+            if (!Grounded)
+            {
+                JetPackFly();
+            }
+        }
 
         if (Input.GetKeyUp(KeyCode.Space))
         {
             if (_chargedJump)
             {
                 Jump(true);
+            }
+            else
+            {
+                Jump(false);
             }
 
             ExitBoost();
@@ -137,45 +153,33 @@ public class EthanPlayerMovement : MonoBehaviour
 
 
         if (Input.GetKeyDown(KeyCode.LeftShift) && _dashTimer <= 0)
-            {
-                StartCoroutine(Dash());
-            }
+        {
+            StartCoroutine(Dash());
+        }
 
-            // Update velocity from input
-            Vector3 desiredVelocity;
-            float availableAcceleration;
-            if (Dashing)
-            {
-                desiredVelocity = _dashDirection * _dashSpeed;
-                availableAcceleration = _dashAcceleration;
-            }
-            else if(!_turretMode && !Boosting)
-            {
-                desiredVelocity = (transform.forward * vertical + transform.right * horizontal).normalized * MaxSpeed;
-                availableAcceleration = Grounded ? _acceleration : _acceleration * _airControl;
-            }
+        // Update velocity from input
+        Vector3 desiredVelocity;
+        float availableAcceleration;
+        if (Dashing)
+        {
+            desiredVelocity = _dashDirection * _dashSpeed;
+            availableAcceleration = _dashAcceleration;
+        }
+        else if (!Boosting)
+        {
+            desiredVelocity = (transform.forward * vertical + transform.right * horizontal).normalized * MaxSpeed;
+            availableAcceleration = Grounded ? _acceleration : _acceleration * _airControl;
+        }
         else
         {
-            desiredVelocity = new Vector3(0,0,0);
+            desiredVelocity = new Vector3(0, 0, 0);
             availableAcceleration = 0;
+            vertical = 0;
+            horizontal = 0;
         }
-            Vector3 actualVelocity = Vector3.ProjectOnPlane(Velocity, Vector3.up);
-            Velocity += (desiredVelocity - actualVelocity) * availableAcceleration * Time.deltaTime;
-        if (!_turretMode)
-        {
-
-        }
-        else
-        {
-            if (_camControl._mouseSensitivityX == _storedCamX)
-            {
-                _camControl._mouseSensitivityX *= 5;
-            }
-            if(_camControl._mouseSensitivityY == _storedCamY)
-            {
-                _camControl._mouseSensitivityY *= 5;
-            }
-        }
+        Vector3 actualVelocity = Vector3.ProjectOnPlane(Velocity, Vector3.up);
+        Velocity += (desiredVelocity - actualVelocity) * availableAcceleration * Time.deltaTime;
+      
 
 
         // Gravity
@@ -183,9 +187,39 @@ public class EthanPlayerMovement : MonoBehaviour
         if (Dashing || (Grounded && Velocity.y < 0))
             Velocity.y = 0;
 
-          _controller.Move(Velocity * Time.deltaTime);
 
-       //drains energy every fram based on the current state of the player
+
+        if (Boosting)
+        {
+            if (Velocity.x < -0.1f || Velocity.x > 0.1f)
+            {
+                Velocity.x = Mathf.Lerp(Velocity.x, 0, 6 * Time.deltaTime);
+            }
+
+            if (Velocity.z > 0.1f || Velocity.z < -0.1f)
+            {
+                Velocity.z = Mathf.Lerp(Velocity.z, 0, 6 * Time.deltaTime);
+            }
+
+            float _distance = Vector3.Distance(_cam.position, _camCrouch.position);
+            if (_distance > 0.1f)
+            {
+                _cam.position = Vector3.Lerp(_cam.position, _camCrouch.position, 3 * Time.deltaTime);
+            }
+
+        }
+        else
+        {
+            float _distance = Vector3.Distance(_cam.position, _camOrigin.position);
+            if (_distance > 0.1f)
+            {
+                _cam.position = Vector3.Lerp(_cam.position, _camOrigin.position, 8 * Time.deltaTime);
+            }
+        }
+
+        _controller.Move(Velocity * Time.deltaTime);
+
+        //drains energy every fram based on the current state of the player
         CheckEnergyUpdate();
     }
 
@@ -202,89 +236,99 @@ public class EthanPlayerMovement : MonoBehaviour
     private void Jump(bool isCharged)
     {
         float jumpHeight;
-        if(isCharged) 
+        if (isCharged)
         {
             jumpHeight = _chargedJumpHeight;
+            _jumpAudio.Play();
+            _springAnim.SetTrigger("spring");
+            _springAnim.SetBool("charging", false);
         }
         else
         {
             jumpHeight = _jumpVelocity;
         }
-        
+
         Velocity.y += jumpHeight;
         _jumpTimer = _jumpCooldown;
     }
 
+    private void JetPackFly()
+    {
+       
+        Velocity += Vector3.up * _jetPackStrength * Time.deltaTime;
+        _currentEnergy -= _energyCostBoost * Time.deltaTime; 
+        
+    }
+
+
     private void Boost()
     {
-        //if (_currentEnergy <= 0) return;
-
-        //Velocity.y += _boostAcceleration * Time.deltaTime;
-        //Velocity.y = Mathf.Clamp(Velocity.y, float.NegativeInfinity, _boostVelocity);
-
-        Boosting = true;
-
         if (Grounded)
-        {
-        _currentJumpHoldTime += Time.deltaTime;
+        {    
+            _currentJumpHoldTime += Time.deltaTime;       
+            Boosting = true;
+            _springAnim.SetBool("charging", true);    
 
         }
-        else
+            else
+            {
+                ExitBoost();
+            }
+
+            if (_currentJumpHoldTime >= _jumpHoldTime)
+            {
+                _chargedJump = true;
+            }      
+
+    }
+        private void ExitBoost()
         {
-            ExitBoost();
+
+            _springAnim.SetBool("charging", false);
+            _springAnim.ResetTrigger("spring");
+            _currentJumpHoldTime = 0;
+            _chargedJump = false;
+            Boosting = false;
+        }
+
+        private IEnumerator Dash()
+        {
+            //if (_currentEnergy < _energyCostDash * _dashDuration) yield break;
+
+            SoundManager(_dashSound);
+            Dashing = true;
+            Vector3 input = transform.forward * Input.GetAxisRaw("Vertical") + transform.right * Input.GetAxisRaw("Horizontal");
+            if (input.magnitude > .1f)
+                _dashDirection = input.normalized;
+            else if (Vector3.ProjectOnPlane(Velocity, Vector3.up).magnitude > MaxSpeed / 2)
+                _dashDirection = Vector3.ProjectOnPlane(Velocity, Vector3.up).normalized;
+            else
+                _dashDirection = transform.forward;
+            yield return new WaitForSeconds(_dashDuration);
+            Dashing = false;
+            _dashTimer = _dashCooldown;
+        }
+
+        private IEnumerator Transform(float time)
+        {
+            //this is where the player will transform from aerial to turret mode!
+            yield return new WaitForSeconds(time);
+            Debug.Log("Transformed");
         }
 
 
-        if(_currentJumpHoldTime >= _jumpHoldTime)
+
+        private void SoundManager(AudioSource _audio)
         {
-            _chargedJump = true;
+            //randomly play sound at different pitch and volume
+
+
+            float randomPitch = Random.Range(_minPitch, _maxPitch);
+            float randomVolume = Random.Range(_minSoundVolume, _maxSoundVolume);
+
+            _audio.pitch = randomPitch;
+            _audio.volume = randomVolume;
+            _audio.Play();
         }
-    }
-
-    private void ExitBoost()
-    {
-        _currentJumpHoldTime = 0;
-        _chargedJump = false;
-        Boosting = false;
-    }
-
-    private IEnumerator Dash()
-    {
-        //if (_currentEnergy < _energyCostDash * _dashDuration) yield break;
-
-        SoundManager(_dashSound);
-        Dashing = true;
-        Vector3 input = transform.forward * Input.GetAxisRaw("Vertical") + transform.right * Input.GetAxisRaw("Horizontal");
-        if (input.magnitude > .1f)
-            _dashDirection = input.normalized;
-        else if (Vector3.ProjectOnPlane(Velocity, Vector3.up).magnitude > MaxSpeed / 2)
-            _dashDirection = Vector3.ProjectOnPlane(Velocity, Vector3.up).normalized;
-        else
-            _dashDirection = transform.forward;
-        yield return new WaitForSeconds(_dashDuration);
-        Dashing = false;
-        _dashTimer = _dashCooldown;
-    }
-
-    private IEnumerator Transform(float time)
-    {
-        //this is where the player will transform from aerial to turret mode!
-        yield return new WaitForSeconds(time);
-        Debug.Log("Transformed");
-    }
-
-
-
-    private void SoundManager(AudioSource _audio)
-    {
-        //randomly play sound at different pitch and volume
-
-
-        float randomPitch = Random.Range(_minPitch, _maxPitch);
-        float randomVolume = Random.Range(_minSoundVolume, _maxSoundVolume);
-
-        _audio.pitch = randomPitch;
-        _audio.volume = randomVolume;
-        _audio.Play();
-    }
+    
 }
